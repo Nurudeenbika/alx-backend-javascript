51e1d34d06d68571222a50738c9b897ddaa6ec73
Hassan Nurudeen Osikpemhi <93295795+Nurudeenbika@users.noreply.github.com>
2023-04-11 11:32:55 +0100

    Update 0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 /**
  * Displays a message on the STDOUT.
  * @param {String} msg The message to display.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Hassan Nurudeen <https://github.com/Nurudeenbika>
  */
 const displayMessage = (msg) => {
   console.log(msg);
